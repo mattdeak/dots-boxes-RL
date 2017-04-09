afa346f05a31227cf57fa1be847e9dd6e047a1e8
matthew <mwoolnerdeakos@gmail.com>
2017-04-09 03:43:14 -0400

    added enumerator for clarity

--- dots_and_boxes.py
+++ dots_and_boxes.py
@@ -6,6 +6,7 @@ Created on Sat Apr  8 21:52:43 2017
 """
 
 import numpy as np
+from enum import Enum
 
 class DotsAndBoxes():
     """The main environment for a dots and boxes game"""
@@ -21,15 +22,23 @@ class DotsAndBoxes():
         self._player1 = None
         self._player2 = None
         self.player_turn = 2   
-        self.score = [0,0]
+        self.score = {'player1':0,'player2':0}
         self.action_list = []
         self.turn = 1 #Which player's turn is it
         self.manual = 0 #No manual player
-        self.rewards = {'win':1,'loss':-1,'draw':0}
-        self.N = 0
-        self.S = 1
-        self.E = 2
-        self.W = 3
+        self.reward_dictionary = {'win':1,'loss':-1,'draw':0}
+        self.rewards = {1:0,2:0}
+        
+        
+    class Sides(Enum):
+        N = 0
+        NORTH = 0
+        S = 1
+        SOUTH = 1
+        E = 2
+        EAST = 2
+        W = 3
+        WEST = 3
     
     @property    
     def player1(self):
@@ -61,7 +70,7 @@ class DotsAndBoxes():
           
     
     def step(self,action):
-        """Takes an action and returns the next game state."""
+        """Takes an action and changes the game state."""
         reward = 0
         
         #If the player is trying to act on the terminal state
@@ -81,8 +90,9 @@ class DotsAndBoxes():
         
         #Add a wall where the action dictates
         self.state[action] = 1
-        if self.made_box(action):
-            self.score[self.player_turn - 1] += 1
+        action_score = self.made_box(action)
+        if self.made_box(action) > 0:
+            self.score[self.player_turn - 1] += action_score
         else:
             self.switch_turn()
         
@@ -94,17 +104,22 @@ class DotsAndBoxes():
         """Plays a game"""
         pass
     
+    def reward_payout(self):
+        self._player1.receive_reward()
+    
     def convert_to_wall(self,state):
         """Converts a specific game state array to the wall it represents"""
         row,column,side = state
-        if side == self.N:
+        if side == self.Sides.N:
             return row * self.size + column
-        elif side == self.S:
+        elif side == self.Sides.S:
             return (row + 1) * self.size + column
-        elif side == self.E:
+        elif side == self.Sides.E:
             return (self.size * (self.size + 1)) + row * self.size + column
-        elif side == self.W:
+        elif side == self.Sides.W:
             return (self.size * (self.size + 1)) + row * self.size + (column + 1)
+        else:
+            raise ValueError("Can't convert state to wall. 3rd Dimension must be range [0-3]")
             
     
     def convert_to_state(self,wall_number):
@@ -115,18 +130,18 @@ class DotsAndBoxes():
             cell_column = wall_number % self.size
             cell_row = wall_number // self.size
             if cell_row != 0:
-                cells.append([cell_row - 1, cell_column, self.S])
+                cells.append([cell_row - 1, cell_column, self.Sides.S])
             if cell_row != self.size:
-                cells.append([cell_row, cell_column, self.N])
+                cells.append([cell_row, cell_column, self.Sides.N])
         #Otherwise the wall is E-W        
         else:
             cell_row = (wall_number-(self.size * (self.size + 1)))//(self.size + 1)
             cell_column = wall_number % (self.size + 1)
             if cell_column != 0:
-                cells.append([cell_row, cell_column - 1, self.E])
+                cells.append([cell_row, cell_column - 1, self.Sides.E])
                 
             if cell_column != self.size:
-                cells.append([cell_row, cell_column, self.W])
+                cells.append([cell_row, cell_column, self.Sides,W])
                 
         return cells
                 
